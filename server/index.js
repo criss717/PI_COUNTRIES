fef06cf7f69077bb7306550829c85c5fcb08a488
Cristian Guzman <mono-717@hotmail.com>
2023-08-20 21:45:43 +0200

    20-08-23

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const server = require("./src/server");
 const { conn,Country,Activity } = require('./src/db.js');
 const PORT = 3001;
 const saveJsonInDB = require("../server/src/midlewares/saveJsonInBD")
-const dbJson=require('./api/db.json')
 
 conn.sync({ force: true })
-  .then(()=>saveJsonInDB(dbJson,Country))
+  .then(()=>saveJsonInDB(Country))
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
--- server/src/midlewares/saveJsonInBD.js
+++ server/src/midlewares/saveJsonInBD.js
@@ -1,18 +1,24 @@
-module.exports=  async (dataJson,model)=>{ // llenamos los datos en la base de datos
-    dataJson.countries.forEach(async (elem) => {
-        try {
-            await model.create({
-                id:elem.cca3,              
-                name:elem.name.common,  
-                imageFlag:elem.flags.png,
-                continents:elem.continents[0],
-                capital:elem.capital && elem.capital[0] || null,
-                subRegion:elem.subregion,
-                area:elem.area,
-                population:elem.population,
-            })            
-        } catch (error) {
-            console.log(error);
-        }
-    });
+const axios=require('axios')
+module.exports=  async (model)=>{ // llenamos los datos en la base de datos
+    try {
+        const {data} = await axios('http://localhost:5000/countries')  // pedimos la info a la API    
+        data.length>0 && data.forEach(async (elem) => {
+            try {
+                await model.create({
+                    id:elem.cca3,              
+                    name:elem.name.common,  
+                    imageFlag:elem.flags.png,
+                    continents:elem.continents[0],
+                    capital:elem.capital && elem.capital[0] || null,
+                    subRegion:elem.subregion,
+                    area:elem.area,
+                    population:elem.population,
+                })            
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
